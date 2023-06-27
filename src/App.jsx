f7f4caad6766fdec710907c9685b8f7762ef0595
Freddy <122462360+FreddZas@users.noreply.github.com>
2023-06-26 23:09:55 -0500

    Galletas_de_la_Fortuna

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,55 @@
-import './App.css'
-import LastPage from './components/LastPage.jsx'
-import phrases from './data/phrases.json'
-
-import { useState } from 'react'
+import './App.css';
+import LastPage from './components/LastPage.jsx';
+import phrases from './data/phrases.json';
+import clickSound from "./assets/sounds/soundBtn.mp3";
+import { useRef } from 'react';
+import { useState } from 'react';
 
 function App() {
 
-  const backgrounds = ["url(/fondos/fondo1.jpg)", "url(/fondos/fondo2.jpg)", "url(/fondos/fondo3.jpg)", "url(/fondos/fondo4.jpg)"]
+  const backgrounds = ["url(/fondos/fondo1.jpg)", "url(/fondos/fondo2.jpg)", "url(/fondos/fondo3.jpg)", "url(/fondos/fondo4.jpg)"];
 
-  const [index, setIndex] = useState(0)
+  const audioRef = useRef(null);
+
+  const [index, setIndex] = useState(0);
 
   const changePhrase = () => {
     const randomPhrase = Math.floor((Math.random() * phrases.length));
-       setIndex(randomPhrase)
-      }
+       setIndex(randomPhrase);
+      };
 
   const [image, setImage] = useState(0);
 
   const changeBackground = () => {
     const randomImage = Math.floor((Math.random() * backgrounds.length));
     setImage(randomImage)
-  }
+  };
 
   const superButton = () => {
+    audioRef.current.play()
     changeBackground();
     changePhrase();
-  }
+  };
 
   document.body.style.backgroundImage = `${backgrounds[image]}` ;
+  
 
     return (
         <>  
             <LastPage
             data = { phrases [index]}
             backgrounds = { backgrounds[image]}
+            
             />
-            <div className='tryLuck'>
+            <audio ref={audioRef}>
+              <source src={clickSound} type="audio/mp3" />
+            </audio>
+            <div className='btn_container'>
               <button onClick={superButton}>PRUEBA TU SUERTE</button>
             </div>
         </>
     )
 };
   
-export default App
+export default App;
 
